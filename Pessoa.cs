b976f07c1ae4865496252c049c9bf0552ebf1e77
Matheus-Rodrigues <matheus.martins.r.a@gmail.com>
2019-05-29 10:27:43 -0300

    Create Pessoa.cs

---
+++ Pessoa.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoyerApp.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Contato { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+    }
+    
+
+}
